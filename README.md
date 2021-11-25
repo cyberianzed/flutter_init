e0da17cca05bd17a9e998e51d2c73722557a6f13
Jose K James <josekjames5@gmail.com>
2021-11-25 19:31:30 +0530

    Initial flutter template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # flutter_init
+
+A new Flutter project.
+
+This project is a intial template for a Flutter application.
---
+++ lib/theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'constants.dart';
+
+// Our light/Primary Theme
+ThemeData themeData(BuildContext context) {
+  return ThemeData(
+    appBarTheme: appBarTheme,
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: Colors.white,
+    backgroundColor: Colors.white,
+    iconTheme: IconThemeData(color: kBodyTextColorLight),
+    accentIconTheme: IconThemeData(color: kAccentIconLightColor),
+    primaryIconTheme: IconThemeData(color: kPrimaryIconLightColor),
+    textTheme: GoogleFonts.latoTextTheme().copyWith(
+      bodyText1: TextStyle(color: kBodyTextColorLight),
+      bodyText2: TextStyle(color: kBodyTextColorLight),
+      headline4: TextStyle(color: kTitleTextLightColor, fontSize: 32),
+      headline1: TextStyle(color: kTitleTextLightColor, fontSize: 80),
+    ),
+    colorScheme: ColorScheme.light(
+      secondary: kSecondaryLightColor,
+      // on light theme surface = Colors.white by default
+    ).copyWith(secondary: kAccentLightColor),
+  );
+}
+
+// Dark Them
+ThemeData darkThemeData(BuildContext context) {
+  return ThemeData.dark().copyWith(
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: Color(0xFF0D0C0E),
+    appBarTheme: appBarTheme,
+    backgroundColor: kBackgroundDarkColor,
+    iconTheme: IconThemeData(color: kBodyTextColorDark),
+    accentIconTheme: IconThemeData(color: kAccentIconDarkColor),
+    primaryIconTheme: IconThemeData(color: kPrimaryIconDarkColor),
+    textTheme: GoogleFonts.latoTextTheme().copyWith(
+      bodyText1: TextStyle(color: kBodyTextColorDark),
+      bodyText2: TextStyle(color: kBodyTextColorDark),
+      headline4: TextStyle(color: kTitleTextDarkColor, fontSize: 32),
+      headline1: TextStyle(color: kTitleTextDarkColor, fontSize: 80),
+    ),
+    colorScheme: ColorScheme.light(
+      secondary: kSecondaryDarkColor,
+      surface: kSurfaceDarkColor,
+    ).copyWith(secondary: kAccentDarkColor),
+  );
+}
+
+AppBarTheme appBarTheme = AppBarTheme(color: Colors.transparent, elevation: 0);
